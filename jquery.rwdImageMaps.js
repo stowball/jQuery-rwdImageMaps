df16d2ce8b40cbade2269d1d0e57a7eff24db90d
Matt Stow <matt@mattstow.com>
2012-06-03 13:12:50 +1000

    Fixed resized areas in WebKit as it doesn't know image dimensions until the image has loaded

--- jquery.rwdImageMaps.js
+++ jquery.rwdImageMaps.js
@@ -1,5 +1,5 @@
 /*
-* rwdImageMaps jQuery plugin v1.1
+* rwdImageMaps jQuery plugin v1.2
 *
 * Allows image maps to be used in a responsive design by recalculating the area coordinates to match the actual image size on load and window.resize
 *
@@ -17,35 +17,39 @@
 			$img.each(function() {
 				if (typeof($(this).attr('usemap')) == 'undefined')
 					return;
-					
-				var w, h;
-				// jQuery < 1.6 incorrectly uses the actual image width/height instead of the attribute's width/height
-				if (v < 1.6)
-					w = $(this)[0].getAttribute('width'),
-					h = $(this)[0].getAttribute('height');
-				else
-					w = $(this).attr('width'),
-					h = $(this).attr('height');	
 				
-				var wPercent = $(this).width()/100,
-					hPercent = $(this).height()/100,
-					map = $(this).attr('usemap').replace('#', ''),
-					c = 'coords';
+				var that = this;
+				// Since WebKit doesn't know the height until after the image has loaded, perform everything in an onload copy
+				$('<img />').attr('src', $(that).attr('src')).load(function() {
+					var w, h;
+					// jQuery < 1.6 incorrectly uses the actual image width/height instead of the attribute's width/height
+					if (v < 1.6)
+						w = $(that)[0].getAttribute('width'),
+						h = $(that)[0].getAttribute('height');
+					else
+						w = $(that).attr('width'),
+						h = $(that).attr('height');	
 					
-				$('map[name="' + map + '"]').find('area').each(function() {
-					if (!$(this).data(c))
-						$(this).data(c, $(this).attr(c));
+					var wPercent = $(that).width()/100,
+						hPercent = $(that).height()/100,
+						map = $(that).attr('usemap').replace('#', ''),
+						c = 'coords';
+					
+					$('map[name="' + map + '"]').find('area').each(function() {
+						if (!$(this).data(c))
+							$(this).data(c, $(this).attr(c));
 						
-					var coords = $(this).data(c).split(','),
-						coordsPercent = new Array(coords.length);
-					
-					for (var i = 0; i < coordsPercent.length; ++i) {
-						if (i % 2 === 0)
-							coordsPercent[i] = parseInt(((coords[i]/w)*100)*wPercent);
-						else
-							coordsPercent[i] = parseInt(((coords[i]/h)*100)*hPercent);
-					}
-					$(this).attr(c, coordsPercent.toString());
+						var coords = $(this).data(c).split(','),
+							coordsPercent = new Array(coords.length);
+						
+						for (var i = 0; i < coordsPercent.length; ++i) {
+							if (i % 2 === 0)
+								coordsPercent[i] = parseInt(((coords[i]/w)*100)*wPercent);
+							else
+								coordsPercent[i] = parseInt(((coords[i]/h)*100)*hPercent);
+						}
+						$(this).attr(c, coordsPercent.toString());
+					});
 				});
 			});
 		};
