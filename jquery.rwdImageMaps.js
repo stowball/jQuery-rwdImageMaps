326952f8d9cafae8957d34370eb11ae693b8fb5e
Mathias Bynens <mathias@qiwi.be>
2012-08-31 16:22:23 +0300

    Optimize performance
    Cache jQuery objects instead of creating a new jQuery instance every time.

--- jquery.rwdImageMaps.js
+++ jquery.rwdImageMaps.js
@@ -10,8 +10,8 @@
 */
 (function($) {
 	$.fn.rwdImageMaps = function() {
-		var $img = this,
-			v = parseFloat($.fn.jquery);
+		var $img = this;
+		var v = parseFloat($.fn.jquery);
 		
 		var rwdImageMap = function() {
 			$img.each(function() {
@@ -19,28 +19,30 @@
 					return;
 				
 				var that = this;
+				var $that = $(that);
 				// Since WebKit doesn't know the height until after the image has loaded, perform everything in an onload copy
-				$('<img />').attr('src', $(that).attr('src')).load(function() {
+				$('<img />').attr('src', $that.attr('src')).load(function() {
 					var w, h;
 					// jQuery < 1.6 incorrectly uses the actual image width/height instead of the attribute's width/height
 					if (v < 1.6)
-						w = $(that)[0].getAttribute('width'),
-						h = $(that)[0].getAttribute('height');
+						w = that.getAttribute('width'),
+						h = that.getAttribute('height');
 					else
-						w = $(that).attr('width'),
-						h = $(that).attr('height');	
+						w = $that.attr('width'),
+						h = $that.attr('height');	
 					
-					var wPercent = $(that).width()/100,
-						hPercent = $(that).height()/100,
-						map = $(that).attr('usemap').replace('#', ''),
+					var wPercent = $that.width()/100,
+						hPercent = $that.height()/100,
+						map = $that.attr('usemap').replace('#', ''),
 						c = 'coords';
 					
 					$('map[name="' + map + '"]').find('area').each(function() {
-						if (!$(this).data(c))
-							$(this).data(c, $(this).attr(c));
+						var $this = $(this);
+						if (!$this.data(c))
+							$this.data(c, $this.attr(c));
 						
-						var coords = $(this).data(c).split(','),
-							coordsPercent = new Array(coords.length);
+						var coords = $this.data(c).split(',');
+						var coordsPercent = new Array(coords.length);
 						
 						for (var i = 0; i < coordsPercent.length; ++i) {
 							if (i % 2 === 0)
@@ -48,14 +50,11 @@
 							else
 								coordsPercent[i] = parseInt(((coords[i]/h)*100)*hPercent);
 						}
-						$(this).attr(c, coordsPercent.toString());
+						$this.attr(c, coordsPercent.toString());
 					});
 				});
 			});
 		};
-		rwdImageMap();
-		$(window).resize(function() {
-			rwdImageMap();
-		});
+		$(window).resize(rwdImageMap).trigger('resize');
 	};
 })(jQuery);
